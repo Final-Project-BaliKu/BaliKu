d72aa634f0b9fa5d45411ad810e26e525d58ab1b
darrel2411 <tapsdarrel@gmail.com>
2021-08-20 12:27:03 +0800

    app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 3000;
+const router = require("./router");
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
+app.use(cors());
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
